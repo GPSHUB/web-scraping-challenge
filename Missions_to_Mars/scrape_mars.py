24ec9705d37a211082ac5ca83f71d2990e3e43f1
GiamSigaud <giam_sigaud@hotmail.com>
2020-10-02 13:40:26 -0500

    10.2.20 1:40p CST

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,92 @@
+from bs4 import BeautifulSoup 
+from splinter import Browser
+import pandas as pd 
+import requests 
+
+# Start browser
+def start_browser(): 
+    executable_path = {'executable_path': '/app/.chromedriver/bin/chromedriver'}
+    return Browser('chrome', headless=True, **executable_path)
+
+mars_dict = {}
+
+# MARS NEWS
+def mars_news_scrape():
+    try:        
+        browser = init_browser()
+        url = 'https://mars.nasa.gov/news/'
+        browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        news_title = soup.find('div', class_='bottom_gradient').find('a').text
+        news_p = soup.find('div', class_='rollover_description_inner').text
+        mars_dict['news_title'] = news_title
+        mars_dict['news_paragraph'] = news_p
+        return mars_dict
+    finally:
+        browser.quit()
+
+# Mars image
+def mars_image_scrape():
+    try:
+        url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+        browser.visit(url)
+        html = browser.html
+        url_soup = BeautifulSoup(html, 'html.parser')
+        current_featured_mars_image = browser.find_by_id('full_image')
+        current_featured_mars_image.click()
+        browser.is_element_present_by_text('more info')
+        more_info_2 = browser.links.find_by_partial_text("more info")
+        more_info_2.click()
+        html = browser.html
+        url_soup = BeautifulSoup(html, 'html.parser')
+        img_url = url_soup.find('figure', class_="lede")
+        image = img_url.find('a')["href"]
+        
+        # Concatenate URL
+        img_url = f"https://www.jpl.nasa.gov{image}"
+        img_url
+        # Update Dictionary
+        mars_dict['featured_image_url'] = img_url
+        return mars_dict
+
+    finally:
+        browser.quit()
+
+# Mars Facts
+def mars_facts_scrape():    
+    space_facts_url = 'https://space-facts.com/mars/'
+    tables = pd.read_html(space_facts_url)
+    mars_df = tables[1]
+    mars_df.set_index('Mars - Earth Comparison', inplace=True)
+    mars_df.head()
+    del mars_df['Earth']
+    html_table = mars_df.to_html()
+    mars_dict['mars_facts'] = data
+    return mars_dict
+
+# # MARS HEMISPHERES
+def mars_hemispheres_scrape():
+    try:
+        url = ' https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        result = soup.find_all('div', class_ = "item")
+        for item in result:        
+            title = item.find("div", class_="description")
+            title2 = title.find('a', class_ = "itemLink product-item").find('h3').get_text()
+
+            url = item.find('a', class_ = "itemLink product-item")["href"]
+            full_url = f"https://astrogeology.usgs.gov{url}"
+
+            browser.visit(full_url)
+            html = browser.html
+            url_soup = BeautifulSoup(html, 'html.parser')
+            full_img_url = url_soup.find("div", class_ = "downloads") 
+            result = full_img_url.select_one('ul li a')['href']          
+            hem_img_url = f"{title2} {result}"
+            mars_dict['hem_img_url'] = hem_img_url
+            return mars_dict
+    finally:
+        browser.quit
